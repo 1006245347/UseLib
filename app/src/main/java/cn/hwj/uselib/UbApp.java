e42f8ec0278f6f7202a5f5a6d6a9ec4e6cc7c23e
1006245347@qq.com <1006245347@qq.com>
2022-08-17 13:46:30 +0800

    v3

--- app/src/main/java/cn/hwj/uselib/UbApp.java
+++ app/src/main/java/cn/hwj/uselib/UbApp.java
@@ -9,7 +9,7 @@ public class UbApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        GreeNp.init(this, "6BD8A496699787957A8132C10020A872");
+        GreeNp.init(this, "6BD8A496699787957A8132C10020A872","cccc1");
     }
 }
 
