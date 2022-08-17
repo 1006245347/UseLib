453fb979fb618ef838fb57138e740860495204ba
1006245347@qq.com <1006245347@qq.com>
2022-08-17 15:43:21 +0800

    feat(大坑): 记一次发布的大坑代理引用

--- app/src/main/java/cn/hwj/uselib/MainActivity.java
+++ app/src/main/java/cn/hwj/uselib/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.hwj.np.NpUtils;
+import cn.hwj.sdk.GreeNp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NpUtils.test();
+        GreeNp.trackSearch("6666c");
     }
 }
